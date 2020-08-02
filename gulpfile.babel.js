50b97c866107d642a26b7023e8300d642bea237a
Jae Hoon Yang <didwogns789@naver.com>
2020-08-02 09:05:54 +0900

    webserver Task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,32 @@
 import gulp from "gulp";
 import gPug from "gulp-pug";
 import del from "del";
+import ws from "gulp-webserver";
 
 const routes = {
   pug: {
+    watch: "src/**/*.puge",
     src: "src/*.pug",
     dest: "dist",
   },
 };
 
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+};
+
 const clean = () => del(["dist"]);
+
+const webserver = () =>
+  gulp.src("dist").pipe(ws({ livereload: true, open: true }));
 
 const pug = () =>
   gulp.src(routes.pug.src).pipe(gPug()).pipe(gulp.dest(routes.pug.dest));
 
-export const dev = gulp.series([clean, pug]);
+const prepare = gulp.series([clean]);
+const assets = gulp.series([pug]);
+const live = gulp.series([webserver, watch]);
+
+export const build = gulp.series([prepare, assets]);
+
+export const dev = gulp.series([build, live]);
