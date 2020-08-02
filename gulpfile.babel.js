6093d366aea0f8f7dd390e4bfd7ee76f45c9e36f
Jae Hoon Yang <didwogns789@naver.com>
2020-08-02 09:20:50 +0900

    SCSS Task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,9 @@ import gulp from "gulp";
 import gPug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import minify from "gulp-csso";
 
 const routes = {
   pug: {
@@ -9,10 +12,16 @@ const routes = {
     src: "src/*.pug",
     dest: "dist",
   },
+  scss: {
+    watch: "src/scss/*.scss",
+    src: "src/scss/style.scss",
+    dest: "dist/css",
+  },
 };
 
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.scss.watch, scss);
 };
 
 const clean = () => del(["dist"]);
@@ -23,8 +32,22 @@ const webserver = () =>
 const pug = () =>
   gulp.src(routes.pug.src).pipe(gPug()).pipe(gulp.dest(routes.pug.dest));
 
+const scss = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        flexbox: true,
+        grid: "autoplace",
+        overrideBrowserslist: "last 2 versions",
+      })
+    )
+    .pipe(minify())
+    .pipe(gulp.dest(routes.scss.dest));
+
 const prepare = gulp.series([clean]);
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, scss]);
 const live = gulp.series([webserver, watch]);
 
 export const build = gulp.series([prepare, assets]);
